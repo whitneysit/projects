b70d99b2fa3cbcce01a0b0231b696ed980bcabb3
Wing Lam Sit <wsit@usc.edu>
2017-02-16 10:06:29 -0800

    adding p19

--- project_euler/p19.cpp
+++ project_euler/p19.cpp
@@ -14,7 +14,40 @@ How many Sundays fell on the first of the month during the twentieth century (1 
 
 
 #include <iostream>
+#include <vector> 
 
 using namespace std; 
 
+int main(){
+	int daysInAMonth[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}; 
+	int dayOfTheWeek = 2; //0-6 = Sunday-Saturday 1/01/1901 was a tuesday
+	int currYear= 1901; //
+	//int currMonth = 0; // 0-11 Jan-Dec
+	int numOfSun = 0; 
+	while (currYear != 2001){
+		for (int j = 0; j < 12; j++){
+			if (dayOfTheWeek == 0){
+				numOfSun++; 
+			}
+			if ((j == 1) && (currYear%4==0) && !(currYear%400==0) ){
+				for (int i = 0; i < 29; ++i){
+					dayOfTheWeek++; 
+					dayOfTheWeek = dayOfTheWeek%7; 
+				}
+			}
+			else {
+				for (int i = 0; i < daysInAMonth[j]; ++i){
+					dayOfTheWeek++; 
+					dayOfTheWeek = dayOfTheWeek%7; 
+				}
+			}
 
+		}
+		currYear++; 
+	}
+	cout << numOfSun << endl; 
+	return 0; 
+}
+
+
+ 
